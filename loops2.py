8f93de1cd8bf4261fb48c6e99bd8f63e53e0d561
Steven <stevendg922@gmail.com>
2023-12-15 11:56:04 -0500

    Created basic loop word problems and solutions.

---
+++ loops2.py
@@ -0,0 +1,16 @@
+# loops2.py project
+# 11-14-2023
+# by Steven D. Garcia
+
+# Exercise #1
+
+for i in range(10, 26, 5):
+    print(i, end=" ")
+
+print()
+
+# Exercise #2
+
+for i in range(-3, 21, 3):
+    print(i, end=", ")
+print(i + 3)
